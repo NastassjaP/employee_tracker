af3a5477ca019e872b7601f795a0878d319d31e8
Nastassja <nastassja@platform45.com>
2019-06-19 10:23:34 +0200

    Added delete functionality to employees

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -15,6 +15,13 @@ def create
   redirect_to @division
 end
 
+def destroy
+    @division = Division.find(params[:division_id])
+    @employee = @division.employees.find(params[:id])
+    @employee.destroy
+    redirect_to @division
+  end
+
 private
 def employee_params
   params.require(:employee).permit(:employee_name)
